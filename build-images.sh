9bb102c62368c357a19144827183326b3c78fc15
Martin Otieno <martin@genius.ke>
2025-02-26 18:00:16 +0300

    update Affine to 0.20.2

--- build-images.sh
+++ build-images.sh
@@ -16,7 +16,7 @@ repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 reponame="affine"
 
 # APP_VERSION="stable-c5da8dd"
-APP_VERSION="canary-22f9db6"
+APP_VERSION="stable-2ec7de7"
 
 # Create a new empty container image
 container=$(buildah from scratch)
