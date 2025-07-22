40cc7cafb018a0f2db4d2831f31f87461eaf76b2
Martin Otieno <martin@genius.ke>
2025-07-22 08:14:32 +0300

    revert graphql image name with version tag, as per 0.23.0 changelog

--- build-images.sh
+++ build-images.sh
@@ -17,7 +17,7 @@ reponame="affine"
 
 # APP_VERSION="stable-c5da8dd"
 APP_VERSION="stable-c4cf579"
-GRAPHQL_VERSION="canary-011f92f"
+GRAPHQL_VERSION="0.23.2"
 
 # Create a new empty container image
 container=$(buildah from scratch)
